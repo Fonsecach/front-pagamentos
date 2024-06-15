066234d1296c83297c03162d242493948a3c7b76
Cleiton <chdf@proton.me>
2024-06-14 23:54:49 -0300

    ajustes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import Navigation from './components/navigator';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import ListarPessoas from './components/pages/pessoas';
-import Footer from './components/footer';
 
-
+import AppRoutes from './routes';
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <Navigation></Navigation>
-      </header>
-      <main>
-        <h1>Teste</h1>
-        <div>
-        <ListarPessoas></ListarPessoas>
-        </div>
-        <footer><Footer></Footer></footer>
-      </main>
+      <AppRoutes />
     </div>
   );
 }
